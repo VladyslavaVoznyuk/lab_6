6a84d4d4d377ed1891dc7b27e6aa0b3ee78d7797
VadymLeus <ipz223_lvo@student.ztu.edu.ua>
2024-05-14 15:05:05 +0300

    Split the menu into separate methods

--- lab6/Program.cs
+++ lab6/Program.cs
@@ -14,113 +14,166 @@ class Program
 
         while (true)
         {
-            Console.WriteLine("1. Create Product");
-            Console.WriteLine("2. Add Product to Inventory");
-            Console.WriteLine("3. Update Inventory Product QTY");
-            Console.WriteLine("4. Delete Product");
-            Console.WriteLine("5. View All Products");
-            Console.WriteLine("6. View Inventory");
-            Console.WriteLine("7. Exit");
-            Console.Write("Enter your choice: ");
+            DisplayMenu();
+
             string choice = Console.ReadLine();
 
             switch (choice)
             {
                 case "1":
-                    Console.Write("Enter product name: ");
-                    string name = Console.ReadLine();
-                    Console.Write("Enter product price: ");
-                    double price = Convert.ToDouble(Console.ReadLine());
-                    Console.Write("Enter product quantity: ");
-                    int quantity = Convert.ToInt32(Console.ReadLine());
-
-                    Product product = new Product {Id = idCounter, Name = name, Price = price, Quantity = quantity };
-                    productService.AddProduct(product);
-                    notificationService.SendNotification(product);
-                    productRepository.Add(product);
-
-                    Console.WriteLine("Product added successfully!");
-                    idCounter++;
-
+                    AddNewProduct(productService, notificationService, productRepository, ref idCounter);
                     break;
-
                 case "2":
-                    Console.Write("Enter product id: ");
-                    int id = Convert.ToInt32(Console.ReadLine());
-
-                    inventory.AddProductToInventory(productRepository.GetById(id));
-                    notificationService.SendNotification(productRepository.GetById(id));
-
-                    Console.WriteLine("Product successfully added to inventory!");
-                    idCounter++;
-
+                    AddProductToInventory(inventory, productRepository);
                     break;
-
                 case "3":
-                    Console.Write("Enter product ID to update: ");
-                    int productIdToUpdate = Convert.ToInt32(Console.ReadLine());
-                    int changeQTY = 0;
-                    Product productToUpdate = productRepository.GetById(productIdToUpdate);
-                    if (productToUpdate != null)
-                    {
-                        Console.Write("Enter how to change product quantity: ");
-                        changeQTY = Convert.ToInt32(Console.ReadLine());
-
-                       // productService.UpdateProduct(productToUpdate);
-                        inventory.UpdateInventory(productToUpdate, changeQTY);
-                       // productRepository.Update(productToUpdate);
-
-                        Console.WriteLine("Product updated successfully!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Product not found!");
-                    }
+                    UpdateInventory(inventory, productRepository);
                     break;
-
                 case "4":
-                    Console.Write("Enter product ID to delete: ");
-                    int productIdToDelete = Convert.ToInt32(Console.ReadLine());
-                    Product productToDelete = productRepository.GetById(productIdToDelete);
-                    if (productToDelete != null)
-                    {
-                        productService.RemoveProduct(productIdToDelete);
-                        inventory.RemoveProductFromInventory(productToDelete);
-                        productRepository.Delete(productIdToDelete);
-
-                        Console.WriteLine("Product deleted successfully!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Product not found!");
-                    }
+                    DeleteProduct(productService, inventory, productRepository);
                     break;
-
                 case "5":
-                    Console.WriteLine("All Products:");
                     productRepository.DisplayAllProducts();
                     break;
-
                 case "6":
-                    Console.WriteLine("Inventory:");
-                    foreach (var item in inventory.Products)
-                    {
-                        Console.WriteLine($"Name: {item.Name}, Price: {item.Price}, Quantity: {item.Quantity}");
-                    }
-
+                    DisplayInventory(inventory);
                     break;
-
                 case "7":
                     Console.WriteLine("Exiting...");
                     Environment.Exit(0);
                     break;
-
                 default:
                     Console.WriteLine("Invalid choice!");
                     break;
             }
 
             Console.WriteLine();
+        }
+    }
+
+    static void DisplayMenu()
+    {
+        Console.WriteLine("1. Create Product");
+        Console.WriteLine("2. Add Product to Inventory");
+        Console.WriteLine("3. Update Inventory Product QTY");
+        Console.WriteLine("4. Delete Product");
+        Console.WriteLine("5. View All Products");
+        Console.WriteLine("6. View Inventory");
+        Console.WriteLine("7. Exit");
+        Console.Write("Enter your choice: ");
+    }
+
+    static void AddNewProduct(ProductService productService, EmailNotificationService notificationService, ProductRepository productRepository, ref int idCounter)
+    {
+        Console.Write("Enter product name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter product price: ");
+        double price;
+        while (!double.TryParse(Console.ReadLine(), out price))
+        {
+            Console.WriteLine("Invalid price format. Please enter a valid number.");
+            Console.Write("Enter product price: ");
+        }
+        Console.Write("Enter product quantity: ");
+        int quantity;
+        while (!int.TryParse(Console.ReadLine(), out quantity))
+        {
+            Console.WriteLine("Invalid quantity format. Please enter a valid number.");
+            Console.Write("Enter product quantity: ");
+        }
+
+        Product product = new Product { Id = idCounter, Name = name, Price = price, Quantity = quantity };
+        productService.AddProduct(product);
+        notificationService.SendNotification(product);
+        productRepository.Add(product);
+
+        Console.WriteLine("Product added successfully!");
+        idCounter++;
+    }
+
+    static void AddProductToInventory(Inventory inventory, ProductRepository productRepository)
+    {
+        Console.Write("Enter product id: ");
+        int id;
+        while (!int.TryParse(Console.ReadLine(), out id))
+        {
+            Console.WriteLine("Invalid ID format. Please enter a valid number.");
+            Console.Write("Enter product id: ");
+        }
+
+        Product product = productRepository.GetById(id);
+        if (product != null)
+        {
+            inventory.AddProductToInventory(product);
+            Console.WriteLine("Product successfully added to inventory!");
+        }
+        else
+        {
+            Console.WriteLine("Product not found!");
+        }
+    }
+
+    static void UpdateInventory(Inventory inventory, ProductRepository productRepository)
+    {
+        Console.Write("Enter product ID to update: ");
+        int productIdToUpdate;
+        while (!int.TryParse(Console.ReadLine(), out productIdToUpdate))
+        {
+            Console.WriteLine("Invalid ID format. Please enter a valid number.");
+            Console.Write("Enter product ID to update: ");
+        }
+
+        int changeQTY;
+        Console.Write("Enter how to change product quantity: ");
+        while (!int.TryParse(Console.ReadLine(), out changeQTY))
+        {
+            Console.WriteLine("Invalid quantity format. Please enter a valid number.");
+            Console.Write("Enter how to change product quantity: ");
+        }
+
+        Product productToUpdate = productRepository.GetById(productIdToUpdate);
+        if (productToUpdate != null)
+        {
+            inventory.UpdateInventory(productToUpdate, changeQTY);
+            Console.WriteLine("Product updated successfully!");
+        }
+        else
+        {
+            Console.WriteLine("Product not found!");
+        }
+    }
+
+    static void DeleteProduct(ProductService productService, Inventory inventory, ProductRepository productRepository)
+    {
+        Console.Write("Enter product ID to delete: ");
+        int productIdToDelete;
+        while (!int.TryParse(Console.ReadLine(), out productIdToDelete))
+        {
+            Console.WriteLine("Invalid ID format. Please enter a valid number.");
+            Console.Write("Enter product ID to delete: ");
+        }
+
+        Product productToDelete = productRepository.GetById(productIdToDelete);
+        if (productToDelete != null)
+        {
+            productService.RemoveProduct(productIdToDelete);
+            inventory.RemoveProductFromInventory(productToDelete);
+            productRepository.Delete(productIdToDelete);
+
+            Console.WriteLine("Product deleted successfully!");
+        }
+        else
+        {
+            Console.WriteLine("Product not found!");
+        }
+    }
+
+    static void DisplayInventory(Inventory inventory)
+    {
+        Console.WriteLine("Inventory:");
+        foreach (var item in inventory.Products)
+        {
+            Console.WriteLine($"Name: {item.Name}, Price: {item.Price}, Quantity: {item.Quantity}");
         }
     }
 }
